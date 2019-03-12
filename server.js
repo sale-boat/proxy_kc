f888626e857b9661b673ed6a0eee90dd1653eb2e
khengschew <khengc@gmail.com>
2019-03-12 14:47:47 -0700

    Update proxy to include other components

--- server.js
+++ server.js
@@ -11,15 +11,19 @@ app.use(cors());
 
 app.use(compression());
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use('/files', express.static(`${__dirname}/public`));
+// app.use('/files', express.static(`${__dirname}/public`));
 
 app.get('/:id', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.use('/api/related', proxy({ target: 'http://ec2-54-219-186-15.us-west-1.compute.amazonaws.com/' }));
+
+app.use('/api/reviews', proxy({ target: 'http://ec2-18-209-30-138.compute-1.amazonaws.com/' }));
+
+app.use('/api/products', proxy({ target: 'http://ec2-3-90-25-170.compute-1.amazonaws.com/' }));
 
 app.get('*', (req, res) => {
   res.redirect('/1');
