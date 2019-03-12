69618c06fc2999137bc61a3aab5f5f32832b2acc
khengschew <khengc@gmail.com>
2019-03-12 15:16:54 -0700

    Add new relic

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
-const path = require('path');
-const favicon = require('serve-favicon');
 const proxy = require('http-proxy-middleware');
 
 const app = express();
@@ -10,10 +9,6 @@ const app = express();
 app.use(cors());
 
 app.use(compression());
-
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-// app.use('/files', express.static(`${__dirname}/public`));
 
 app.get('/:id', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
@@ -29,5 +24,4 @@ app.get('*', (req, res) => {
   res.redirect('/1');
 })
 
-// app.listen(80);
 app.listen(3000);
